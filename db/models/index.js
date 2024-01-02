0be2c6a1fd65d4577b655b7dadb679fea0234faf
Jamova01 <jorgemova01@gmail.com>
2024-01-02 09:31:17 -0500

    Initialized and associated Sequelize models.

---
+++ db/models/index.js
@@ -0,0 +1,21 @@
+const { User, UserSchema } = require("./user.model")
+const { Assistant, AssistantSchema } = require("./assistant.model")
+const { Thread, ThreadsSchema } = require("./thread.model")
+const { Message, MessageSchema } = require("./message.model")
+const { Run, RunSchema } = require("./run.model")
+
+function setupModels(sequelize) {
+  User.init(UserSchema, User.config(sequelize))
+  Assistant.init(AssistantSchema, Assistant.config(sequelize))
+  Thread.init(ThreadsSchema, Thread.config(sequelize))
+  Message.init(MessageSchema, Message.config(sequelize))
+  Run.init(RunSchema, Run.config(sequelize))
+
+  User.associate(sequelize.models)
+  Assistant.associate(sequelize.models)
+  Thread.associate(sequelize.models)
+  Message.associate(sequelize.models)
+  Run.associate(sequelize.models)
+}
+
+module.exports = setupModels
