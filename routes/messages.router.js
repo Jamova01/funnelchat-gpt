b28492439607bc12245e1a563a493b91bc4dd5c4
Jamova01 <jorgemova01@gmail.com>
2024-01-02 09:57:53 -0500

    Implemented endpoints for retrieving and creating messages within threads.

---
+++ routes/messages.router.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const MessagesService = require("../services/message.service")
+// const validatorHandler = require('../middlewares/validator.handler');
+
+const router = express.Router()
+const service = new MessagesService()
+
+router.get("/", async (req, res, next) => {
+  try {
+    const body = req.body
+
+    const threadMessages = await service.find(body)
+    res.json(threadMessages)
+  } catch (error) {
+
+  }
+})
+
+router.post("/", async (req, res, next) => {
+  try {
+    const body = req.body
+    const message = await service.create(body)
+    res.status(200).json(message)
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router
