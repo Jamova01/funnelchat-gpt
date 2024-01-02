5b360cc02e7d0f2a4b45f556f3c9fc084499f8dd
Jamova01 <jorgemova01@gmail.com>
2024-01-02 10:10:14 -0500

    Configured Express app with routes and error handling middleware.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('My app');
+const express = require('express');
+const routerApi = require('./routes');
+
+const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+routerApi(app);
+
+app.use(logErrors);
+app.use(ormErrorHandler);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log('Mi port' +  port);
+});
